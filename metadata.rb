380dabd571c958a0df8c1c2adf2ebcae428b9af9
Alexis Vanier <alexis@amonoid.io>
2018-04-22 15:20:47 -0400

    Update short description

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name 'vscode'
 maintainer 'Alexis Vanier'
 maintainer_email 'alexis@amonoid.io'
 license 'Apache-2.0'
-description 'Installs vscode'
+description 'Installs Visual Studio Code'
 long_description 'Installs Visual Studio Code'
 version '0.1.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
