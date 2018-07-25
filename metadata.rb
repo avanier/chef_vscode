ba478af39aaf92b85f5ab02193980e37b2e9a1b6
Marcin Mateusz Hanc <marcin@teknobingo.com>
2018-07-25 15:03:41 +0200

    Enabled use of this cookbook for CentOS.

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,8 @@ long_description 'Installs Visual Studio Code'
 version '0.1.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
-supports 'fedora'
+supports 'centos', '>= 7'
+supports 'fedora', '>= 22'
 
 source_url 'https://github.com/avanier/chef_vscode'
 issues_url 'https://github.com/avanier/chef_vscode/issues'
